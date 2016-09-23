e44f49ec9613ae8b4067dc6785f87a825b4f6e57
charujaiswal <clmjaiswal@gmail.com>
2016-09-23 12:47:22 -0400

    Mac terminal script to resize images

---
+++ image_processing.py
@@ -0,0 +1,20 @@
+import numpy as np
+from PIL import Image
+
+
+'''
+
+Mac terminal script to reduce image sizes:
+
+-get to the directory where the images are (using cd)
+
+sips -Z 100 *.jpeg
+
+- sips is the command being used
+-Z tells it to maintain the aspect ratio
+- 100 is the new size of the image
+- *.jpeg instructs the computer to downsize every image ending in jpeg
+
+'''
+
+
