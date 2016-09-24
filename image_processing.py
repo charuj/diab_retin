925df9817f53272cdfac539fdb4260e94bc44860
charujaiswal <clmjaiswal@gmail.com>
2016-09-24 17:01:13 -0400

    creating an array of all the images' pixels; had to delete some images because the shapes weren't the same as the rest

--- image_processing.py
+++ image_processing.py
@@ -38,16 +38,23 @@ for item in os.listdir("/Users/charujaiswal/PycharmProjects/diab_retin/sample"):
     if not item.startswith('.') and isfile(join(root, item)): # to get rid of the hidden file ".DS_Store"; http://stackoverflow.com/questions/15235823/how-to-ignore-hidden-files-in-python-functions
         img= Image.open(item)
         arr = np.array(img) # at this point each individual array will be 3D
-        pixels= arr.flatten()
-        images_list.append(pixels)
+        pixels1D= arr.flatten()
+        pixels2D= pixels1D.reshape([pixels1D.shape[0], 1]) # turn 1D array into 2D that has shape (#, 1)... helps with later matrix mult
+        pixels2D= np.transpose(pixels2D)
+        images_list.append(pixels2D)
 
-images_array= np.array(images_list)
-print images_array.shape()
-#
-# # Convert images to pixel and rgb values
-# img = Image.open(onlyfiles[2])
-# arr= np.array(img)
-# print arr.shape()
+for i in range(len(images_list)):
+    print images_list[i].shape
 
 
+## FOR SOME REASON resizing the images down didn't give all of the same shape, which then gives issues when vstacking
 
+del images_list[2]
+del images_list[2]
+
+for i in range(len(images_list)):
+    print images_list[i].shape
+
+images_array = np.vstack(images_list)
+
+print images_array.shape  
