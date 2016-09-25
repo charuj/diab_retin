12d1423f1ee46517eb459c97169e14cbf754c157
charujaiswal <clmjaiswal@gmail.com>
2016-09-25 14:52:16 -0400

    trim border; buggy trimmed too much!

---
+++ process_augment.py
@@ -0,0 +1,27 @@
+'''
+
+Additional processing than what is in image_processing.py
+
+What's included:
+
+1. Cropping border from image; note this only really works if the image has a very diff colour than the border. In this case it works
+
+
+'''
+
+import numpy as np
+from PIL import Image, ImageChops
+
+# http://stackoverflow.com/questions/10615901/trim-whitespace-using-pil/10616717#10616717
+def trim(img):
+    bg = Image.new(img.mode, img.size, img.getpixel((0,0))) # get border colour from the top left pixel using getpixel; this is so I don't have to pass it the border colour
+    diff = ImageChops.difference(img, bg) # subtracts a scalar from the
+    diff = ImageChops.add(diff, diff, 2.0, -100)
+    bbox = diff.getbbox()
+    if bbox:
+        return img.crop(bbox)
+
+
+img= Image.open("10_left.jpeg")
+img= trim(img)
+img.show()
