9c2f84fe116d8bf9eb638b87a6f949954e01c2c0
charujaiswal <clmjaiswal@gmail.com>
2016-09-25 22:44:55 -0400

    fixed accuracy (now works!)

--- random_forest.py
+++ random_forest.py
@@ -5,8 +5,8 @@ from sklearn.ensemble import RandomForestClassifier
 X=  pickle.load( open( "file_images_array.p", "rb" ) )
 Y=  pickle.load( open( "sampletarget_labels.p", "rb" ) )
 
-Y= np.delete(Y, (2), axis=0)
-Y= np.delete(Y, (2), axis=0)
+Y= np.delete(Y, (2), axis=0) # 13_left
+Y= np.delete(Y, (2), axis=0) # 13_right
 print Y.shape
 
 x_train= X[:-2]
@@ -22,8 +22,8 @@ print x_train.shape, y_train.shape, x_test.shape, y_test.shape
 clf_rf= RandomForestClassifier()
 clf_rf.fit(x_train,y_train)
 y_pred_rf = clf_rf.predict(x_test)
-#y_pred_rf= y_pred_rf.reshape([y_pred_rf.shape[0], 1])
+y_pred_rf= y_pred_rf.reshape([y_pred_rf.shape[0], 1])
 
-acc_rf = clf_rf.score(y_test, y_pred_rf)
+acc_rf = clf_rf.score(x_test, y_test) #BUG HERE
 print "random forest accuracy: ",acc_rf
 
