7f60227696ce36bda2021189799abad9f9cbac56
charujaiswal <clmjaiswal@gmail.com>
2016-10-03 22:36:23 -0400

    TODOs

---
+++ cnn.py
@@ -0,0 +1,19 @@
+'''
+Using convolutional neural network for classification
+
+Features:
+1. Droput
+
+
+Dependencies:
+Numpy
+Tensorflow
+
+
+
+'''
+
+#TODO: adapt augmentation code so it runs on multiple images (can read through a file and make a list)
+#TODO: pickle the file of the augmentted arrays
+#TODO: Build CNN based on tensorflow tutorial + dropout
+
