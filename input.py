e5756749d02e67e421c1dec99588681c85aab889
charujaiswal <clmjaiswal@gmail.com>
2016-11-01 20:09:36 -0400

    Batch function works!

--- input.py
+++ input.py
@@ -17,35 +17,25 @@ sample_labels= np.reshape(sample_labels, [sample_labels.shape[0],1])
 # Loading array of images
 list_images_array= pickle.load( open( "file_images_array_processed.p", "rb" ) )
 
-def generate_batches(list_images_array, labels, min_queue_examples, batch_size, capacity):
-    # images_tensor= tf.sparse_to_dense(list_images_array)
-    # images_tensor= tf.sparse_to_dense(list_images_array, )
-    # labels_tensors= tf.sparse_to_dense(labels)
-
-    labels= tf.sparse_tensor_to_dense(labels)
-
-    images_batch, labels_batch= tf.train.shuffle_batch([list_images_array, labels], batch_size, capacity=capacity, min_after_dequeue=min_queue_examples)
-
-    return images_batch, tf.reshape(labels_batch, [batch_size])
 
 
-def batch_iter(data, batch_size, num_epochs, shuffle=True):
+def batch(data, labels, batch_size, num_epochs, shuffle=True):
     data_size= len(data)
-    num_batches_per_epoch= int(data_size/ batch_size) + 1
+    num_batches_per_epoch= int(data_size/ batch_size)
     for epoch in range(num_epochs):
         if shuffle:
-            shuffle_indices = np.random.permutation(np.arange(data_size))
-            print shuffle_indices
-            shuffled_data = data[shuffle_indices]
-
+            np.random.shuffle([data, labels])
         else:
-            shuffled_data = data
+            data= data
+            labels= labels
         for batch_num in range(num_batches_per_epoch):
             start_index = batch_num * batch_size
             end_index = min((batch_num + 1) * batch_size, data_size)
-            yield shuffled_data[start_index:end_index]
+            yield data[start_index:end_index], labels[start_index:end_index]
 
 
 
-# images_batch, labels_batch= generate_batches(list_images_array, sample_labels, min_queue_examples=1, batch_size=5, capacity=2)
+batch(list_images_array, sample_labels, 5, 10, shuffle=True)
+
+
 
